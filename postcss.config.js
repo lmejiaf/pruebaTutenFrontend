f8fa4f3fc85f7a092557e10c52a4bdd7aa8dfa4e
lmejiaf <lmejiaf@uninorte.edu.co>
2020-10-23 09:26:47 -0500

    initial commit  y todo configurado

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+
+let tailwind= require('tailwindcss')
+let autoprefixer= require('autoprefixer')
+
+module.exports = {
+    plugins: [
+      // ...
+      tailwind,autoprefixer
+      // ...
+    ]
+  }
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <h1>Tailwind CSS</h1>
+      <h2>Incorporado con PostCss</h2>
+      <button className="btn btn-blue btn-blue:hover">Botón</button>
+      <div className="my-5 flex flex-wrap -mx-2">HOLAAAA</div>
+    </div>
+  );
+};
+
+export default App;
